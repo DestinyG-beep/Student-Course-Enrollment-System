829ffc82a18275fe34b62f5b1954d45c374a91ac
mburugudestinygakenga <mburugugakenga@gmail.com>
2025-01-30 13:32:37 +0300

    made course model

--- models.py
+++ models.py
@@ -7,3 +7,9 @@ class Student(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     enrollments = db.relationship('Enrollment', backref='student', cascade='all, delete-orphan' )
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(10), nullable=False)
+    enrollments = db.relationship('Enrollment', backref='course', cascade='all, delete-orphan')
